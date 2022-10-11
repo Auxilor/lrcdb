ec30f4acd14b6e9a1b82f3bb0b5d3480c1d3ecca
Auxilor <william.favierparsons1@gmail.com>
2022-10-11 19:06:37 +0100

    Added import command, moved to MongoDB

---
+++ components/ImportCommand.js
@@ -0,0 +1,42 @@
+import { Button, Card, Typography } from "@mui/joy"
+import { useState } from "react"
+
+export default function ImportCommand(props) {
+    const config = props.config
+    const close = props.close
+    const command = `/${config.plugin.toLowerCase()} import ${config.id}`
+    const [copied, setCopied] = useState(false)
+
+    return (
+        <Card className="w-1/2 grid grid-rows-2 gap-2">
+            <div className="grid grid-cols-6 gap-4">
+                <Typography className="text-2xl col-start-1 col-span-4">
+                    Run this command:
+                </Typography>
+                <Button
+                    variant="outlined"
+                    onClick={() => {
+                        navigator.clipboard.writeText(command)
+                        setCopied(true)
+                        setTimeout(() => setCopied(false), 5000)
+                    }}
+                >
+                    {copied ? "Copied!" : "Copy"}
+                </Button>
+                <Button
+                    variant="outlined"
+                    onClick={() => {
+                        close()
+                    }}
+                >
+                    Close
+                </Button>
+            </div>
+            <Typography
+                className="bg-slate-200 p-2 rounded-lg h-full font-mono text-md"
+            >
+                {command}
+            </Typography>
+        </Card>
+    )
+}
